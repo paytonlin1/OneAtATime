3285aff3e88712e9970716c9261141c60861e612
payton <plinteq14@icloud.com>
2025-12-11 17:03:44 -0500

    Initial commit

---
+++ src/main/java/net/poayt/oneatatime/OneAtATimeClient.java
@@ -0,0 +1,10 @@
+package net.poayt.oneatatime;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OneAtATimeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/poayt/oneatatime/OneAtATimeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.poayt.oneatatime;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OneAtATimeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
